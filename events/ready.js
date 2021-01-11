aac69f0cced6f859b020ee6397c32d4153df6d3a
emiraliegri3131313131 <76996765+emiraliegri3131313131@users.noreply.github.com>
2021-01-11 18:10:51 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
    client.user.setStatus('dnd') 
-   client.user.setActivity(`♥️ Elminstêr Tarafından Yapıldı! |  | ${client.guilds.cache.size} Sunucu`);
+   client.user.setActivity(`Calm Down DEX Was Here !`);
    console.log(`[ BOT HAZIR ] - [ SUNUCULAR - ${client.guilds.cache.size} ] - [ KULLANICILAR - ${client.guilds.cache
           .reduce((a, b) => a + b.memberCount, 0)
           .toLocaleString()} ]`)
